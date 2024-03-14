dffb0896dee14eba05eb905109d3daa6e02b605d
NTA LE TIEN DAT <ledatdx97@gmail.com>
2024-03-14 15:36:19 +0700

    f2

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,6 @@
 import express from "express";
 
-
-// const cart = require('./routes/cart')
-// const user = require('./routes/user')
+import  { query, validationResult } from "express-validator";
 
 const app = express();
 
@@ -11,33 +9,52 @@ const PORT = process.env.PORT || 3000;
 const mockUser = [
     {
         id:1,
-        name:"A"
+        name:"A",
+        age: 8
     },
     {
         id:2,
-        name:"A B"
+        name:"A B",
+        age: 10
     },
     {
         id:3,
-        name:"BA"
+        name:"BA",
+        age: 9
     },
     {
         id:4,
-        name:"GA"
+        name:"GA",
+        age: 39
     },
     {
         id:5,
-        name:"GAew"
+        name:"GAew",
+        age: 12
     },
     {
         id:6,
-        name:"GAvdvd"
+        name:"GAvdvd",
+        age: 32
     },
     {
         id:7,
-        name:"GAvdsvd"
+        name:"GAvdsvd",
+        age: 83
     },
 ];
+
+const middlewareUserId = (req, res, next) => {
+    const { params : { id }} = req;
+    const parseId = parseInt(id);
+    if(isNaN(parseId)) return res.sendStatus(400);
+    const findIndex = mockUser.findIndex(user => user.id == parseId);
+    console.log(findIndex)
+
+    if (findIndex === -1) return res.sendStatus(404);
+    req.findIndex = findIndex;
+    next();
+}
 
 app.use(express.json());
 app.use(express.urlencoded())
@@ -47,32 +64,40 @@ app.use((req, res, next) => {
     next();
 })
 
-// app.use('/api/v1/cart', cart);
-// app.use('/api/v1/user', user);
-
 app.get('/', (req, res) => {
     res.status(201).send({msg : "hello"})
 })
 
-app.get('/api/user', (req, res) => {
-    console.log(req.query)
-    const { filter, value } = req.query
+app.get(
+    '/api/user',
+    query('filter').isString().notEmpty().isLength({ min:3, max:30 }),
+    (req, res) => {
+        const result = validationResult(req)
+        console.log(result)
+        const { filter, value } = req.query
 
-    if(filter && value) return res.send([
-        mockUser.filter(u => u[filter].includes(value))
-    ])
+        if(filter && value) return res.send([
+            mockUser.filter(u => u[filter].includes(value))
+        ])
+        return res.send(mockUser)
+    }
+)
 
-    return res.send(mockUser)
+app.get('/api/user/:id', middlewareUserId, (req, res) => {
+    const { findIndex } = req;
+    return res.send(mockUser[findIndex]);
 })
 
-app.get('/api/user/:id', (req, res) => {
-    console.log(req.params)
-    const parseId = parseInt(req.params.id);
-    if(isNaN(parseId)) return res.status(400).send({ msg: 'Bad Request Invaild ID'});
-    const findUser = mockUser.find(u => u.id === parseId)
+app.put('/api/user/:id', middlewareUserId, (req, res) => {
+    const { body, findIndex } = req;
+    mockUser[findIndex] = {id: parseId, ...body};
+    
+    return res.sendStatus(200);
+})
 
-    if(!findUser) return res.sendStatus(404);
-    return res.send(findUser);
+app.delete('/api/user/:id', middlewareUserId, (req, res) => {
+    const { findIndex } = req;
+    return res.sendStatus(200);
 })
 
 app.listen(PORT, () => console.log(`run expess on server port ${PORT}`))
